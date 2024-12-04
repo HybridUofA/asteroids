cc86dd22948541431e2e4b5033d5c6df1ac39507
HybridUofA <benjaminlukeedger@gmail.com>
2024-12-04 08:37:03 -0600

    circleshape module added, constants updated

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -13,6 +15,8 @@ def main():
                 return
         screen.fill((0,0,0))
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick()/1000
 
 
 if __name__ == "__main__":
